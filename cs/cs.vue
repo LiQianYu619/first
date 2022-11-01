6980e918d7a8b8ac9f03bbfe91a2f4361ae4b83f
LQY <LQY@qianyu.com>
2022-11-01 19:26:52 +0800

    第一次修改

--- cs/cs.vue
+++ cs/cs.vue
@@ -1,6 +1,6 @@
 <template>
 	<view style="margin: 40% 0 60% 0;">
-		<!-- //第一次本地修改 -->
+		<!-- 第一次本地的修改 -->
 		<u-notify ref="uNotify" message="Hi uView"></u-notify>
 		<view style="display: flex;flex-direction: column;align-items: center;margin-bottom: 5%;">
 			<u-avatar :src="avatarUrl" size="250rpx"></u-avatar>
